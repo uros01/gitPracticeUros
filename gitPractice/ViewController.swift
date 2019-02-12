5adce7752116a16ac939819e5ac13df4551b3078
student18 <student18@student18s-macbook-air.local>
2019-02-12 12:12:47 -0600

    COMMIT VESION

--- gitPractice/ViewController.swift
+++ gitPractice/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }// addeed one more comment for testing propersessssssssssiuugyyg
     
-
+// sgged one morere comebhsygyatfdtfdtftstfyudcd
 
 }
 
